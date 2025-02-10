a034648035cedae080552a8388e72a32fb7f9ff4
John Lin (jclin) <jclin@longleaf-login1.its.unc.edu>
2025-02-10 13:01:50 -0500

    removing gene module

--- gene.py
+++
@@ -1,8 +0,0 @@
-class CandGene:
-    def __init__(self, name, chrom, start, end):
-        self.name = name
-        self.chr = chrom
-        self.s = start
-        self.e = end
-    def __hash__(self):
-        return hash(self.name)
